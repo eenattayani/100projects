540fff4482e0f1257530ed27e15a61124a9a2144
eenattayani <enryfirmana@gmail.com>
2023-04-05 12:50:27 +0700

    revisi index.html

---
+++ 9-tugas-alam/4-javascript/8-javascript-array-of-object.js
@@ -0,0 +1,6 @@
+// Buatlah sebuah data array of objek bertemakan apa saja.
+// Setelah itu buatlah 4 proses berikut untuk data array of object yang sudah dibuat.
+// 1. Create => Menambahkan 1 atau lebih objek baru di dalam data array of object
+// 2. Read => Mengakses property di dalam salah 1 object di data array of object
+// 3. Update => Mengubah isi salah 1 property di dalam object data array of object
+// 4. Delete => Menghapus salah 1 object di dalam data array of object
